294f1aa3249cfc4740b1d9096e86b1a0e9a30291
Ian Grant Jeffries <ian@housejeffries.com>
2020-01-14 19:35:34 -0500

    Add select example

---
+++ examples/Select/Main.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Concur.Core (Widget)
+import Concur.Replica
+import Control.Monad.IO.Class
+import Data.Maybe
+import Data.Text (Text)
+import Replica.VDOM (HTML)
+
+import Prelude hiding (div)
+
+import qualified Data.Text as T
+
+selectApp :: Maybe Text -> Widget HTML a
+selectApp mSelected = do
+  newSelection <- div []
+    [ select [onChange]
+        [ option [disabled True, selected True, hidden True] [text "Select a choice..."]
+        , option [value "A"] [text "Choose A"]
+        , option [value "B"] [text "Choose B"]
+        , option [value "C"] [text "Choose C"]
+        ]
+    , p [] [text ("Currently selected: " <> fromMaybe "none" mSelected)]
+    ]
+  selectApp (Just (eventToText newSelection))
+  where
+    eventToText :: BaseEvent -> Text
+    eventToText = targetValue . target
+
+main :: IO ()
+main = runDefault 8080 "Select" (selectApp Nothing)
