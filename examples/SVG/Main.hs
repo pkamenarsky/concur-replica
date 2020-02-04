28acddbd752f1047db1d72a24210b0d5ea3a7244
Ian Grant Jeffries <ian@housejeffries.com>
2020-02-04 18:38:03 -0500

    Change example to use Event with SVG

--- examples/SVG/Main.hs
+++ examples/SVG/Main.hs
@@ -4,11 +4,10 @@ module Main where
 
 import Concur.Core (Widget)
 import Concur.Replica
-import Control.Concurrent
-import Control.Monad.IO.Class
 import Prelude hiding (div)
 import Replica.VDOM (HTML)
 
+import qualified Concur.Replica.Events as SE
 import qualified Concur.Replica.SVG as S
 import qualified Concur.Replica.SVG.Props as SP
 
@@ -19,12 +18,11 @@ app :: Widget HTML a
 app = do
   _ <-
     div []
-    [ p [] [text "You should see a black rectangle for one second..."]
-    , S.svg [width "200", height "200", SP.version "1.1", xmlns]
+    [ p [] [text "Click to change size and color"]
+    , S.svg [SE.onClick, width "200", height "200", SP.version "1.1", xmlns]
         [ S.rect [SP.x "10", SP.y "10", width "180", height "180"]
             []
         ]
-    , liftIO (threadDelay 2000000)
     ]
   div []
     [ p [] [text "Now you should see a small red rectangle."]
